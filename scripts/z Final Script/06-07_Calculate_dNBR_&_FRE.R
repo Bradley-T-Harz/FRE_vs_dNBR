fa464c037b6b9b5fe552d9e5aac987e0b84e2bcc
Bradley Harz <bharz@student.cccs.edu>
2025-07-31 11:31:32 -0600

    Add placeholder for 06-07_Calculate_dNBR_&_FRE pipeline

---
+++ scripts/z Final Script/06-07_Calculate_dNBR_&_FRE.R
@@ -0,0 +1,5 @@
+# 🔜  Pipeline forthcoming
+# This script will calculate and compare dNBR & FRE,
+# but the code is not yet ready for public release—
+# pending our manuscript submission.
+# Stay tuned for updates!
